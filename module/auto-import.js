4351f0318118c6586139a41455156b0c48be2aa4
MrVauxs <mrvauxs@gmail.com>
2022-05-22 19:42:43 +0200

    1.1.1

--- module/auto-import.js
+++ module/auto-import.js
@@ -1,7 +1,11 @@
+let version = 111
+
 Hooks.on("init", () => {
 	game.settings.register("pf2e-jb2a-macros", "imported", {
 		scope: "world",
-		config: false,
+		config: true,
+		name: "Imported Contents",
+		hint: "Whether or not you have imported the contents of the module using the pop-up when you enabled the module.\nDisable to have them imported again automatically.",
 		type: Boolean,
 		default: false
 	});
@@ -21,11 +25,11 @@ async function importAllMacros() {
 	const module = game.modules.get("pf2e-jb2a-macros");
 	let scenes = null;
 	let actors = null;
-	for ( let p of module.packs ) {
-		const pack = game.packs.get("pf2e-jb2a-macros."+p.name);
+	for (let p of module.packs) {
+		const pack = game.packs.get("pf2e-jb2a-macros." + p.name);
 		await pack.importAll();
-		if ( p.entity === "Macro" ) macros = game.folders.getName(p.label);
-		if ( p.entity === "Actor" ) actors = game.folders.getName(p.label);
+		if (p.entity === "Macro") macros = game.folders.getName(p.label);
+		if (p.entity === "Actor") actors = game.folders.getName(p.label);
 	}
 	return game.settings.set("pf2e-jb2a-macros", "imported", true);
 }
