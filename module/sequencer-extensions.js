d1345d41ab0465f468a0f3a7579e748499d62849
mrvauxs <mrvauxs@gmail.com>
2022-12-06 21:14:24 +0000

    add sequencer extensions

--- module/sequencer-extensions.js
+++ module/sequencer-extensions.js
@@ -1,26 +1,12 @@
 /*
-	"esmodules": [
-		"module/sequencer-extensions.js"
-	],
+    "esmodules": [
+        "module/sequencer-extensions.js"
+    ],
 */
 Hooks.once('sequencer.ready', () => {
-    class pulseInAndOut extends Sequence.prototype {
-
-        constructor(inSequence) {
-            super(inSequence);
-        }
-
-        pulseInAndOut({ from, to, duration, pingPong }) {
-            this.loopProperty("spriteContainer", "scale.x", { from: from ?? 0.9, to: to ?? 1.1, duration: duration ?? 5000, pingPong: pingPong ?? true })
-            this.loopProperty("spriteContainer", "scale.y", { from: from ?? 0.9, to: to ?? 1.1, duration: duration ?? 5000, pingPong: pingPong ?? true })
-            return this;
-        }
-
-        run() {
-            // this.loopProperty("spriteContainer", "scale.x", { from: from ?? 0.9, to: to ?? 1.1, duration: duration ?? 5000, pingPong: pingPong ?? true })
-            // this.loopProperty("spriteContainer", "scale.y", { from: from ?? 0.9, to: to ?? 1.1, duration: duration ?? 5000, pingPong: pingPong ?? true })
-        }
-    }
-
-    Sequencer.SectionManager.registerSection("pf2e-jb2a-macros", "effect", pulseInAndOut, true)
+    Sequencer.Presets.add("pf2eAnimations.breathe", (effect, args) => {
+        return effect
+            .loopProperty("spriteContainer", "scale.x", { from: 0.9, to: 1.1, duration: args?.duration ?? 3000, pingPong: true, ease: "easeInOutSine" })
+            .loopProperty("spriteContainer", "scale.y", { from: 0.9, to: 1.1, duration: args?.duration ?? 3000, pingPong: true, ease: "easeInOutSine" });
+    });
 })
