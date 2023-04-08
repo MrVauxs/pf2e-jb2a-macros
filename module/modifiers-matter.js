b253d3929b170f2ebba0311caeabe5e812cee3b4
mrvauxs <mrvauxs@gmail.com>
2023-04-08 23:12:24 +0000

    strict modifiers matter

--- module/modifiers-matter.js
+++ module/modifiers-matter.js
@@ -7,15 +7,35 @@ self.pf2eAnimations.modifiersMatter = Hooks.on("modifiersMatter", ({
     const rollingToken = chatMessage.token ?? canvas.tokens.placeables.find(tok => tok.actor === rollingActor)
     if (!rollingToken) return pf2eAnimations.debug("No token found for the Modifier Matter animation.", data);
 
+    const combinedMenu = Object.keys(AutomatedAnimations.AutorecManager.getAutorecEntries()).map((key) => {
+        const menus = AutomatedAnimations.AutorecManager.getAutorecEntries()
+        if (Array.isArray(menus[key])) {
+            return menus[key]
+        }
+    }).flat()
+
     significantModifiers.forEach(({ name, significance }) => {
-        AutomatedAnimations.playAnimation(
-            rollingToken,
-            {
-                name: `Modifiers Matter: ${name} (${significance})`
-            },
-            {
-                targets: [targetedToken]
-            }
-        )
+        pf2eAnimations.debug(`Modifiers Matter:`, `${name} (${significance})`);
+
+        let animationName = false
+        if (combinedMenu.find((x) => x?.label === `Modifiers Matter: ${name} (${significance})`)) {
+            animationName = `Modifiers Matter: ${name} (${significance})`
+        } else if (combinedMenu.find((x) => x?.label === `Modifiers Matter: ${name}`)) {
+            animationName = `Modifiers Matter: ${name}`
+        }
+
+        if (!name) {
+            return pf2eAnimations.debug(`Modifiers Matter | No Animation Found for:`, `${name} (${significance})`);
+        } else {
+            AutomatedAnimations.playAnimation(
+                rollingToken,
+                {
+                    name: animationName
+                },
+                {
+                    targets: [targetedToken]
+                }
+            )
+        }
     });
 });
